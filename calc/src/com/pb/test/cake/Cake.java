8fdc043aba845704f94a728702597dcc75c7a380
natalia.tanatarova <natalia.tanatarova@sigma.software>
2019-11-17 16:58:02 +0200

    improvement in Cakes

--- calc/src/com/pb/test/cake/Cake.java
+++ calc/src/com/pb/test/cake/Cake.java
@@ -1,59 +1,88 @@
 package com.pb.test.cake;
 
 class Cake implements Eatable {
-    static int numberOfCakesInTheWorld = 0;
-    private int a;
+    static int numCakesInTheWorld = 0;
+    int sugarInGrams;
 
-    Cake(int a) {
-        this.a = a;
+    Cake() {
+        numCakesInTheWorld++;
+        sugarInGrams = 0;
     }
 
-    public void makeCake(int b) {
-        System.out.println("i made a cake " + a + " " + b);
-        numberOfCakesInTheWorld++;
+    public void addSomeSugar(int grams) {
+        sugarInGrams += grams;
     }
 
     public void setEaten() {
-        numberOfCakesInTheWorld--;
+        numCakesInTheWorld--;
     }
 
     public static void main(String[] args) {
-        testNumCakes();
+        testCakes();
         testInstanceOf();
+        testInterface();
     }
 
-    private static void testNumCakes() {
-        Cake sweetCake = new Cake(12);
+    private static void testCakes() {
+        Cake sweetCake = new Cake();
         Muffin smallMuffin = new Muffin();
 
-        sweetCake.makeCake(2);
-        smallMuffin.makeCake(3);
-        System.out.println("total cakes: " + Cake.numberOfCakesInTheWorld);
-
+        sweetCake.addSomeSugar(20);
+        sweetCake.addSomeSugar(4);
+        smallMuffin.addSomeSugar(30);
         smallMuffin.setEaten();
-        System.out.println("total cakes: " + Cake.numberOfCakesInTheWorld);
+        System.out.println("total cakes: " + Cake.numCakesInTheWorld);
+        System.out.println("sugar in cake " + sweetCake.sugarInGrams);
+        System.out.println("sugar in muffin " + smallMuffin.sugarInGrams);
+    }
+
+    private static void testInterface() {
+        Eatable food = new Cake();
+        food.setEaten();
+        Eatable f2 = new Muffin();
+        f2.setEaten();
+        PartlyEatable bigFood = new VeryBigCake();
+        bigFood.setPartlyEaten(50);
+        bigFood.setPartlyEaten(10);
+        System.out.println("cakes: " + Cake.numCakesInTheWorld);
+        bigFood.setPartlyEaten(0);
+        System.out.println("cakes: " + Cake.numCakesInTheWorld);
     }
 
     private static void testInstanceOf() {
-        Cake sweetCake = new Cake(12);
+        Cake sweetCake = new Cake();
         Muffin smallMuffin = new Muffin();
+        PartlyEatable wow = new VeryBigCake();
         System.out.println(sweetCake instanceof Cake);
         System.out.println(sweetCake instanceof Eatable);
-        System.out.println(sweetCake instanceof Muffin);
         System.out.println(smallMuffin instanceof Cake);
         System.out.println(smallMuffin instanceof Eatable);
-        System.out.println(smallMuffin instanceof Muffin);
+        System.out.println(wow instanceof Cake);
+        System.out.println(wow instanceof Eatable);
+        System.out.println(wow instanceof PartlyEatable);
     }
 }
 
 class Muffin extends Cake {
-    Muffin() {
-        super(13);
+
+    @Override
+    public void addSomeSugar(int value) {
+        super.addSomeSugar(value / 2);
+    }
+}
+
+class VeryBigCake extends Cake implements PartlyEatable {
+    int percentRemaining;
+
+    VeryBigCake() {
+        percentRemaining = 100;
     }
 
     @Override
-    public void makeCake(int b) {
-        System.out.println("i made a muffin " + b);
-        numberOfCakesInTheWorld++;
+    public void setPartlyEaten(int percentRemaining) {
+        this.percentRemaining = percentRemaining;
+        if (percentRemaining == 0) {
+            setEaten();
+        }
     }
 }
