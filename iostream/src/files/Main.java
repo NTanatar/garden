42d752c91de72474be236f74102fe1372b6001fc
natalia.tanatarova <natalia.tanatarova@sigma.software>
2019-12-10 17:45:22 +0200

    RandomAccessFile

--- iostream/src/files/Main.java
+++ iostream/src/files/Main.java
@@ -1,9 +1,6 @@
 package files;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
@@ -25,7 +22,28 @@ public class Main {
         }
     }
 
+    private static void randomAccess() {
+        try {
+            RandomAccessFile rand = new RandomAccessFile("output2.txt", "rw");
+            double[] values = {2.3, 1.6, 4.7, 8.9};
+            long[] pos = new long[4];
+
+            for (int i = 0; i < 4; i++) {
+                pos[i] = rand.getFilePointer();
+                rand.writeDouble(values[i]);
+                System.out.println("write " + i + " ("+ pos[i] + ") :  " + values[i]);
+            }
+            for (int i = 3; i >= 0; i--) {
+                rand.seek(pos[i]);
+                System.out.println("read " + i + " ("+ pos[i] + ") -> " + rand.readDouble());
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static void main(String[] args) {
         copyFile("src\\files\\input1.txt", "output1.txt");
+        randomAccess();
     }
 }
