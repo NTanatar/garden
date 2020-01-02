0e24a41c8d05ef30040a2ab6c7f8cd8afbb4336e
natalia.tanatarova <natalia.tanatarova@sigma.software>
2020-01-02 19:42:12 +0200

    use Map when declaring, initialize map with known operations

--- calc/src/com/pb/test/calc/MyOpFactory.java
+++ calc/src/com/pb/test/calc/MyOpFactory.java
@@ -7,36 +7,26 @@ import com.pb.test.calc.operation.OpPlus;
 import com.pb.test.math.OperationNotFoundException;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MyOpFactory implements OperationFactory {
 
-    private HashMap<String, Operation> operations = new HashMap<String, Operation>();
+    private static Map<String, Operation> operations;
+
+    static {
+        operations = new HashMap<>();
+        operations.put("+", new OpPlus());
+        operations.put("-", new OpMinus());
+        operations.put("*", new OpMul());
+        operations.put("/", new OpDiv());
+    }
 
     @Override
     public Operation getOpInstance(String id) throws OperationNotFoundException {
         if (operations.containsKey(id)) {
-            System.out.println("found in cache: " + id);
             return operations.get(id);
+        } else {
+            throw new OperationNotFoundException();
         }
-        Operation op;
-        switch (id) {
-            case "+":
-                op = new OpPlus();
-                break;
-            case "-":
-                op = new OpMinus();
-                break;
-            case "*":
-                op = new OpMul();
-                break;
-            case "/":
-                op = new OpDiv();
-                break;
-            default:
-                throw new OperationNotFoundException();
-        }
-        System.out.println("created: " + id);
-        operations.put(id, op);
-        return op;
     }
 }
