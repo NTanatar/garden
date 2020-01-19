c32fbad887eeea6a9eecc00d09cd185685c72250
natalia.tanatarova <natalia.tanatarova@sigma.software>
2020-01-19 21:18:31 +0200

    scheduled execution

---
+++ threads/src/Scheduled.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Scheduled {
+
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Hi there");
+            }
+        }, 0,2, TimeUnit.SECONDS);
+    }
+}
