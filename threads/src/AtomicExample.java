6e9c0c6151983daecb0f2e3dcaacf3e9682de154
natalia.tanatarova <natalia.tanatarova@sigma.software>
2020-01-24 17:22:29 +0200

    AtomicInteger, IntStream.range, Runnable with lambda

---
+++ threads/src/AtomicExample.java
@@ -0,0 +1,26 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class AtomicExample {
+    public static void main(String[] args) {
+        AtomicInteger atomicInt = new AtomicInteger(0);
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        IntStream.range(0, 1000).forEach(i -> {
+                Runnable task = () ->
+                    atomicInt.updateAndGet(n -> n + 5);
+                executor.submit(task);
+            });
+        executor.shutdown();
+
+        try {
+            executor.awaitTermination(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {}
+
+        System.out.println(atomicInt.get());
+    }
+}
