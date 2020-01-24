56a7272f944b1fa8982a07e3d823616dabb62c47
natalia.tanatarova <natalia.tanatarova@sigma.software>
2020-01-24 16:30:21 +0200

    BlockingQueue interface in produce-consume example

--- threads/src/ProduceConsumeTest.java
+++ threads/src/ProduceConsumeTest.java
@@ -1,9 +1,17 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
 public class ProduceConsumeTest {
 
     public static void main(String args[]) {
-        // SynchronizedQueue q = new SynchronizedQueue();
-        QueueWithLock q = new QueueWithLock();
-        new Producer(q);
-        new Consumer(q);
+        SharedQueue q1 = new SynchronizedQueue();
+        SharedQueue q2 = new QueueWithLock();
+        SharedQueue q3 = new QueueB();
+
+        new Producer(q3);
+        new Consumer(q3);
     }
 }
